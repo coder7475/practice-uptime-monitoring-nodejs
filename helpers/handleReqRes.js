3f5e7ab8e30f61262b752c6a8619580006396a7a
coder7475 <robiulhossain7475@gmail.com>
2023-11-18 12:04:21 +0600

    code refactor: create a helper moduler called handleReqRes

---
+++ helpers/handleReqRes.js
@@ -0,0 +1,45 @@
+// dependencies
+const url = require('url');
+const { StringDecoder } = require('node:string_decoder');
+
+// object scafolding
+const handler = {};
+
+handler.handleReqRes = (req, res) => {
+  // request handling 
+  // get the url and parse it 
+  const parsedUrl = url.parse(req.url, true); // true gives full pathname with queries
+  // console.log(parsedUrl);
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/gm, "");
+  const queryObj = parsedUrl.query;
+  // console.log(trimmedPath);
+
+  // find http method
+  const method = req.method.toLowerCase();
+  // console.log(method);
+  const headerObj = req.headers;
+  // console.log(headerObj);
+  // console.log(queryObj);
+
+  // post method + sent data streaming + how to decode buffer
+  const decoder = new StringDecoder("utf-8");
+  let realData = "";
+  // data streaming
+  req.on('data', (buffer) => {
+    realData += decoder.write(buffer);
+  })
+  // stream end
+  req.on('end', () => {
+    realData += decoder.end();
+
+    console.log(realData);
+
+    // response handling 
+    res.end("Uptime Monitoring server is running");
+  })
+
+}
+
+
+module.exports = handler;
