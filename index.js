73ae910fce71965373f871efd504f3cb822f9edf
coder7475 <robiulhossain7475@gmail.com>
2023-12-07 18:38:56 +0600

    feat: add an environments js file

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 // dependencies
 const http = require('http');
 const { handleReqRes } = require('./helpers/handleReqRes');
+const environment = require('./helpers/environments')
 
 // app object - module scaffolding
 const app = {};
@@ -18,8 +19,8 @@ app.config = {
 // create server
 app.createServer = () => {
   const server = http.createServer(app.handleReqRes);
-  server.listen(app.config.port, () => {
-    console.log(`Listening to port ${app.config.port}`);
+  server.listen(environment.port, () => {
+    console.log(`Listening to port ${environment.port} on ${environment.envName}`);
   })
 }
 
