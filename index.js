0ca3a1c96b460d1394657b84c9342b7a65bd5322
coder7475 <robiulhossain7475@gmail.com>
2023-12-13 19:23:26 +0600

    feat: delete method of file system

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const data = require('./lib/data');
 const app = {};
 
 // testing file system
-data.update('test', 'newFile', { name: "America", language: "English"} ,(err) => {
+data.delete('test', 'newFile',(err) => {
   console.log(err);
 })
 
